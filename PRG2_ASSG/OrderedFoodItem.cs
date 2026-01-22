590aea6add68c7f3c1b67758905122832e98ec9f
Dg Muhammad Aqil Md Alias <s10272951@connect.np.edu.sg>
2026-01-22 18:16:32 +0800

    "Created new class"

---
+++ PRG2_ASSG/OrderedFoodItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_ASSG
+{
+    internal class OrderedFoodItem
+    {
+    }
+}
